abcbf37ffb0bd3aeaf24ac1e34c8b4b5b3962a73
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2023-12-14 13:10:29 +0200

    Add files via upload

---
+++ Exam/src/api.js
@@ -0,0 +1,64 @@
+import { userHelper } from "./userHelper.js"
+
+
+const baseUrl = "http://localhost:3030/"
+
+async function requester(url, method, data) {
+    const options = {
+        method,
+        headers: {}
+
+    }
+
+    const userData = userHelper.getUserData();
+
+    if (userData) {
+        options.headers['x-authorization'] = userData.accessToken;
+    }
+    if (data) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    }
+    try {
+        const response = await fetch(baseUrl + url, options);
+        if (!response.ok) {
+            const err = await response.json();
+            throw new Error(err.message);
+        }
+
+        if (response.status === 204) {
+            return response;
+        }
+    
+        return response.json();
+
+
+    }
+    catch (err) {
+        throw alert("err");
+
+    }
+}
+
+async function get(url) {
+    return await requester(url, "GET");
+}
+
+async function post(url, data) {
+    return await requester(url, "POST", data);
+}
+
+async function put(url, data) {
+    return await requester(url, "PUT", data);
+}
+
+async function del(url) {
+    return await requester(url, "DELETE");
+}
+
+export const api = {
+    get,
+    post,
+    put,
+    del,
+}
